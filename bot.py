0e01d2207b747c40557b6baf5caf49239c0008f4
avtotrainer <trainer@din.ge>
2023-08-15 12:55:32 +0400

    update bot

--- bot.py
+++ bot.py
@@ -4,7 +4,11 @@ bot=telebot.TeleBot('6450641365:AAEJyLYrQJkq_HUgmftauH8uOwVpx7krHuo')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'ვაა, ბარო {message.from_user.first_name} {message.from_user.last_name}')
+    bot.send_message(message.chat.id, f'<b>ვაა, ბარო</b> {message.from_user.first_name} {message.from_user.last_name}',parse_mode='HTML')
+
+@bot.message_handler(commands=['help'])
+def help(message):    
+    bot.send_message(message.chat.id, '<b>მე ვარ</b> - <i>მომავლის ბოტი</i> ',parse_mode='HTML')
 
 @bot.message_handler()
 def echo(message):
