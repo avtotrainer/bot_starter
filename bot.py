6a16f7359c0133fd000aa09b1709fba850c2a627
avtotrainer <trainer@din.ge>
2023-08-15 13:19:58 +0400

    update

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 bot=telebot.TeleBot('6450641365:AAEJyLYrQJkq_HUgmftauH8uOwVpx7krHuo')
 
@@ -10,8 +11,34 @@ def start(message):
 def help(message):    
     bot.send_message(message.chat.id, '<b>მე ვარ</b> - <i>მომავლის ბოტი</i> ',parse_mode='HTML')
 
-@bot.message_handler()
-def echo(message):
-    bot.send_message(message.chat.id, f'{message}')
 
-bot.polling(non_stop=True)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+@bot.inline_handler(lambda query: True)
+def inline_query(query):
+    try:
+        # ინლაინ ღილაკის შექმნა
+        keyboard = types.InlineKeyboardMarkup()
+        button = types.InlineKeyboardButton(text="დააკლიკეთ აქ!", callback_data="button_pressed")
+        keyboard.add(button)
+
+        # Создаем инлайн-результат с кнопкой
+        result = types.InlineQueryResultArticle(
+            id='1', title="დააკლიკეთ!",
+            input_message_content=types.InputTextMessageContent(message_text="თქვენ დააკლიკეთ ღილაკზე!"),
+            reply_markup=keyboard
+        )
+
+        bot.answer_inline_query(query.id, [result])
+
+    except Exception as e:
+        print(e)
+
+
+
+# bot.polling(non_stop=True)
+
+bot.infinity_polling()
