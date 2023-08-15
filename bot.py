2fcdca61790a390211d678de158a86c78ad55665
avtotrainer <trainer@din.ge>
2023-08-15 09:55:38 +0400

    bot-1

--- bot.py
+++ bot.py
@@ -4,6 +4,10 @@ bot=telebot.TeleBot('6450641365:AAEJyLYrQJkq_HUgmftauH8uOwVpx7krHuo')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'ვაა, ბარო')
+    bot.send_message(message.chat.id, f'ვაა, ბარო {message.from_user.first_name} {message.from_user.last_name}')
+
+@bot.message_handler()
+def echo(message):
+    bot.send_message(message.chat.id, f'{message}')
 
 bot.polling(non_stop=True)
